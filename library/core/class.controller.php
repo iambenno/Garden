vanilla/vanilla

commit db0a70a9b0a9a3b2b3e58cb36ee0a869a0ae752f

CSS fixes to themes. Changed Controller::Permission to redirect user to login if they are not signed in.

library/core/class.controller.php
@@ -973,10 +973,13 @@ class Gdn_Controller extends Gdn_Pluggable {
     */
    public function Permission($Permission, $JunctionID = '', $FullMatch = TRUE) {
       $Session = Gdn::Session();
-      if (!$Session->CheckPermission($Permission, $JunctionID, $FullMatch)) {
-         // TODO: Make this work with different delivery types.
+      if (!$Session->IsValid())
+         Redirect(Gdn::Authenticator()->SignInUrl($this->SelfUrl));
+
+      // TODO: Make this work with different delivery types.
+      if (!$Session->CheckPermission($Permission, $JunctionID, $FullMatch))
          Redirect($this->Routes['DefaultPermission']);
-      }
+
    }
 
    /**

applications/garden/design/admin.css
@@ -978,6 +978,9 @@ table.SelectionGrid td {
 	padding: 16px;
 	vertical-align: top;
 }
+table.Themes td {
+   min-width: 235px;
+}
 table.Themes td h4 {
 	padding: 6px 0;
 }
@@ -988,8 +991,54 @@ table.SelectionGrid td.EmptyCol,
 table.SelectionGrid td.LastCol {
 	border-left: 1px solid #C0E7F5;
 }
-
-
+table.SelectionGrid td:hover {
+   background: #F2FBFF;
+}
+table.SelectionGrid td.EmptyCol:hover {
+   background: none;
+}
+table.SelectionGrid h4 {
+   font-size: 16px;
+   color: #02455B;
+   text-shadow: 0 1px 0 #fff;
+}
+table.SelectionGrid em {
+   display: block;
+   padding: 3px 0 8px;
+   font-style: oblique;
+}
+table.SelectionGrid img {
+	margin-top: 12px;
+}
+table.SelectionGrid div.Buttons {
+   float: right;
+   width: 70px;
+   text-align: center;
+}
+table.SelectionGrid div.Buttons a {
+   display: block;
+   padding: 2px 4px;
+   margin: 4px 0;
+}
+table.Themes td.HasPreview div.Buttons {
+	padding-top: 59px;
+}
+table.Themes div.Buttons a {
+	margin: 8px 0 0;
+}
+table.SelectionGrid dl {
+	padding: 0 0 10px;
+}
+table.SelectionGrid dl dt {
+	display: inline;
+   color: #000;
+   border-right: 1px solid #C0E7F5;
+   padding: 4px 6px 4px 0;
+	margin: 0 4px 0 0;
+}
+table.SelectionGrid dl dd {
+	display: inline;
+}
 
 /* =================================================================== Footer */
 #Foot {

applications/garden/views/settings/themes.php
@@ -101,16 +101,10 @@ printf(
             $Col = 0;
          }
          $ColClass .= $Active ? ' Enabled' : '';
+         $ColClass .= $PreviewImage ? ' HasPreview' : '';
          ?>
             <td class="<?php echo $ColClass; ?>">
                <?php
-                  if ($PreviewImage) {
-                     echo Anchor('<img src="'.Asset('/themes/'.$ThemeFolder.'/'.$PreviewImage).'" alt="'.$ThemeName.'" height="180" width="240" />',
-                        'garden/settings/previewtheme/'.$ThemeFolder,
-                        '',
-                        array('target' => '_top')
-                     );
-                  }
                   echo '<h4>';
                      echo $ThemeUrl != '' ? Url($ThemeName, $ThemeUrl) : $ThemeName;
                      if ($Version != '')
@@ -120,26 +114,26 @@ printf(
                         $Info .= sprintf('by %s', $AuthorUrl != '' ? Anchor($Author, $AuthorUrl) : $Author);
       
                   echo '</h4>';
-                  echo '<div class="FilterMenu">';
-                  echo Anchor('Apply', 'garden/settings/themes/'.$ThemeFolder.'/'.$Session->TransientKey(), '', array('target' => '_top'));
-                  echo '<span>|</span>';
-                  echo Anchor('Preview', 'garden/settings/previewtheme/'.$ThemeFolder, '', array('target' => '_top'));
-                  echo '</div>';
-                  echo ArrayValue('Description', $ThemeInfo, '&nbsp;');
-      
+                  
+                  $Description = ArrayValue('Description', $ThemeInfo);
+                  if ($Description)
+                     echo '<em>'.$Description.'</em>';
+                     
                   $RequiredApplications = ArrayValue('RequiredApplications', $ThemeInfo, FALSE);
                   if (is_array($RequiredApplications)) {
-                     echo Translate('Requires: ');
-                  }
-                  $i = 0;
-                  if (is_array($RequiredApplications)) {
-                     if ($i > 0)
-                        echo ', ';
-                     
+                     echo '<dl>
+                        <dt>'.Translate('Requires').'</dt>
+                        <dd>';
+
+                     $i = 0;
                      foreach ($RequiredApplications as $RequiredApplication => $VersionInfo) {   
-                        printf(Gdn::Translate('%1$s Version %2$s'), $RequiredApplication, $VersionInfo);
+                        if ($i > 0)
+                           echo ', ';
+                           
+                        printf(Gdn::Translate('%1$s %2$s'), $RequiredApplication, $VersionInfo);
                         ++$i;
                      }
+                     echo '</dl>';
                   }
                   
                   if ($Upgrade) {
@@ -150,6 +144,19 @@ printf(
                         );
                      echo '</div>';
                   }
+
+                  if ($PreviewImage) {
+                     echo Anchor('<img src="'.Asset('/themes/'.$ThemeFolder.'/'.$PreviewImage).'" alt="'.$ThemeName.'" height="112" width="150" />',
+                        'garden/settings/previewtheme/'.$ThemeFolder,
+                        '',
+                        array('target' => '_top')
+                     );
+                  }
+
+                  echo '<div class="Buttons">';
+                  echo Anchor('Apply', 'garden/settings/themes/'.$ThemeFolder.'/'.$Session->TransientKey(), 'Button', array('target' => '_top'));
+                  echo Anchor('Preview', 'garden/settings/previewtheme/'.$ThemeFolder, 'Button', array('target' => '_top'));
+                  echo '</div>';
                ?>
             </td>
             <?php
@@ -159,7 +166,7 @@ printf(
    }
    // Close the row if it wasn't a full row.
    if ($Col > 0)
-      echo '<td class="LastCol"'.($Col == 1 ? ' colspan="2"' : '').'>&nbsp;</td></tr>';
+      echo '<td class="LastCol EmptyCol"'.($Col == 1 ? ' colspan="2"' : '').'>&nbsp;</td></tr>';
    ?>
       </tbody>
    </table>

iambenno/Garden

commit 5ff9fe40040859414d1bba712ec0fc8bbd9bdce1

Changed Permission function to only check if the session is valid after CheckPermission returns FALSE. Change db0a70a9b0a9a3b2b3e58cb36ee0a869a0ae752f broke guests from being able to do anything that they had permissions too (such as view discussions) as calling IsValid first checked if UserID>0.

library/core/class.controller.php
@@ -973,12 +973,15 @@ class Gdn_Controller extends Gdn_Pluggable {
     */
    public function Permission($Permission, $JunctionID = '', $FullMatch = TRUE) {
       $Session = Gdn::Session();
-      if (!$Session->IsValid())
-         Redirect(Gdn::Authenticator()->SignInUrl($this->SelfUrl));
 
       // TODO: Make this work with different delivery types.
-      if (!$Session->CheckPermission($Permission, $JunctionID, $FullMatch))
-         Redirect($this->Routes['DefaultPermission']);
+      if (!$Session->CheckPermission($Permission, $JunctionID, $FullMatch)) {
+        if (!$Session->IsValid()) {
+           Redirect(Gdn::Authenticator()->SignInUrl($this->SelfUrl));
+        } else {
+          Redirect($this->Routes['DefaultPermission']);
+        }
+      }
 
    }
 